05dba92c2a1fb4393b56df6cf3bdafea2be2da5d
Valentin <shakhov.valentin@gmail.com>
2025-08-03 17:18:51 +0200

    readme

---
+++ README.md
@@ -0,0 +1,11 @@
+## Description
+
+User management dashboard with frontend (Angular) and backend (Spring boot)
+
+## Instructions
+
+Run with
+
+`docker-compose up`
+
+Access: http://localhost
--- frontend/src/app/dashboard/dashboard.ts
+++ frontend/src/app/dashboard/dashboard.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import {RouterLink, RouterOutlet} from '@angular/router';
 import {CommonModule} from '@angular/common';
 import {MatSidenavModule} from '@angular/material/sidenav';
